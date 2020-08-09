f046d2196b23d0a90bf98631b3b4b72732f04b3a
Ailin Tobin <ailintobin@gmail.com>
2020-08-09 16:57:18 +0000

    customized admin

--- retreats/admin.py
+++ retreats/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Retreat, Category
 
 # Register your models here.
-admin.site.register(Retreat)
-admin.site.register(Category)
+
+class RetreatAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Retreat, RetreatAdmin)
+admin.site.register(Category, CategoryAdmin)
