e137c08260f44e59efb8119c457497e8276176b0
Ailin Tobin <ailintobin@gmail.com>
2020-08-09 17:07:37 +0000

    added retreat view and templates

--- retreats/views.py
+++ retreats/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Retreat
 
 # Create your views here.
+
+
+def all_retreats(request):
+    """ A view to show all retreats, including sorting and search queries """
+
+    retreats = Retreat.objects.all()
+
+    context = {
+        'retreats': retreats,
+    }
+    
+    return render(request, 'retreats/retreats.html', context)
