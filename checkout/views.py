f7bdb962fd1c58e3cf14e85036052b38c6552ec2
Ailin Tobin <ailintobin@gmail.com>
2020-08-15 15:01:41 +0000

    added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HFP3MLhEJj4oqoF08H3KoswWWzvlSd8AgqdCUs8S2xCdP8fj6TjhUfsTM8o69qXTl8QDXJFsueDmdIJutpLsfuu00euaS4cFJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
