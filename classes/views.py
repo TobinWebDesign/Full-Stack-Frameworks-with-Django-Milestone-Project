216a3ae8e1024d8dc4c44069f8b0cfba12132983
Ailin Tobin <ailintobin@gmail.com>
2020-08-19 20:57:55 +0000

    added classes views and templates

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Class
 
 # Create your views here.
+
+def all_classes(request):
+    """ A view to show all classes """
+
+    classes = Class.objects.all()
+
+    context = {
+        'classes': classes,
+    }
+
+    return render(request, 'classes/classes.html', context)
