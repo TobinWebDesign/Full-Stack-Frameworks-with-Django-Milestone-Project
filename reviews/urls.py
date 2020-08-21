f7022fd7ed6d21c0a5525bf5ba50737b08ed201c
Ailin Tobin <ailintobin@gmail.com>
2020-08-21 19:21:13 +0000

    updated reviews form and fixed typo in views.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url('', views.all_reviews, name='reviews'),
-    url('add_review', views.add_reviews, name='add_reviews'),
+    url('add_review', views.add_review, name='add_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 
 from .models import Review
+from .forms import ReviewForm
 
 def all_reviews(request):
 
@@ -17,21 +18,7 @@ def all_reviews(request):
 
 def add_review(request):
     """ Add a review to the store """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
-
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, request.FILES)
-        if form.is_valid():
-            review = form.save()
-            messages.success(request, 'Yey! Successfully added review!')
-            return redirect(reverse('review_detail', args=[review.sku]))
-        else:
-            messages.error(request, 'Opps! Failed to add review. Please ensure the form is valid.')
-    else:
-        form = ReviewForm()
-
+    form = ReviewForm()
     template = 'reviews/add_review.html'
     context = {
         'form': form,
