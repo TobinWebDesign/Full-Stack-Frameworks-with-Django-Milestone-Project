273c634000c75296fb028cbc60d93162e931af77
Ailin Tobin <ailintobin@gmail.com>
2020-08-21 16:54:50 +0000

    started work on add_reviews by taking templates from the retreats folder

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url('', views.all_reviews, name='reviews'),
+    url('add_review', views.add_reviews, name='add_reviews'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -14,3 +14,27 @@ def all_reviews(request):
 
     
     return render(request, 'reviews/reviews.html', context)
+
+def add_review(request):
+    """ Add a review to the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            review = form.save()
+            messages.success(request, 'Yey! Successfully added review!')
+            return redirect(reverse('review_detail', args=[review.sku]))
+        else:
+            messages.error(request, 'Opps! Failed to add review. Please ensure the form is valid.')
+    else:
+        form = ReviewForm()
+
+    template = 'reviews/add_review.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
