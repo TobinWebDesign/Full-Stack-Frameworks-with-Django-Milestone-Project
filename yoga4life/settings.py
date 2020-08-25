2574fbecaca17084e5ed884d17a0c6c4c36b6db3
Ailin Tobin <ailintobin@gmail.com>
2020-08-25 21:42:39 +0000

    changed debug ot True

--- yoga4life/settings.py
+++ yoga4life/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = Truegit 
 
 ALLOWED_HOSTS = ['yoga4life.herokuapp.com', 'localhost']
 
