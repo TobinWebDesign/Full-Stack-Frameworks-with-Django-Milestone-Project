4b320f540f3c94771bdc0c4e6c1e83f8101cd121
Ailin Tobin <ailintobin@gmail.com>
2020-08-25 10:34:08 +0000

    updated secretkey and debug settings

--- yoga4life/settings.py
+++ yoga4life/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3j%8=#wmp^t!%n))bh^mhv+hth5et+sb13^e^$id@$c)s37h#x'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['yoga4life.herokuapp.com', 'localhost']
 
