c6d7375a39675678c3ebfc43f0dcee6675539435
Ailin Tobin <ailintobin@gmail.com>
2020-08-25 21:23:32 +0000

    updated the class time to a dropdown choice for better admin ux

--- classes/models.py
+++ classes/models.py
@@ -32,6 +32,14 @@ class Class(models.Model):
         ('6', 'Saturday')
     )
 
+    YOGA_CLASS_TIMES = (
+        ('1', '06:00'),
+        ('2', '08:00'),
+        ('3', '12:00'),
+        ('4', '18:00'),
+        ('5', '20:00')       
+    )
+
     level = models.ForeignKey('Level', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
@@ -40,8 +48,8 @@ class Class(models.Model):
     dificulty = models.DecimalField(max_digits=6, decimal_places=0, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    day = models.CharField(max_length=10, choices=DAYS_OF_THE_WEEK)
-    time = models.DateTimeField(null=True, blank=True)
+    day = models.CharField(max_length=10, choices=DAYS_OF_THE_WEEK, null=True, blank=True)
+    time = models.CharField(max_length=10, choices=YOGA_CLASS_TIMES, null=True, blank=True)
 
     def __str__(self):
         return self.name
